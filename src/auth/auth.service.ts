eb9903aab217bd472f7789d1cd390ef708096b60
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-19 22:39:10 +0500

    Now we have to assign permissions to user and admin based on their 'role' means which crud operations they can perform

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,36 @@
-import { Permissions } from './permissions.decorator';
+import { Permission, Permissions } from './permissions.decorator';
 import { UsersService } from './../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AdminService } from 'src/Admin/admin.service';
+import { permission } from 'process';
+import { UserWithPermissions } from './userWithPermission.dto';
 
 @Injectable()
 export class AuthService {
 
 constructor(private userService:UsersService, private jwtService: JwtService, private adminService:AdminService){}
 
-async validateUser(username: string, password: string): Promise<any> {
+async validateUser(username: string, password: string): Promise<UserWithPermissions | null> {
   // First, try to find the user in the user repository
   const user = await this.userService.findOne(username);
   if (user && await bcrypt.compare(password, user.password)) {
-    const { password, ...result } = user;
+    const { password, ...result } = user as UserWithPermissions;
+
+    if(user.role==='admin'){
+      result.permissions=[
+        Permission.GENERAL_ADMIN_PERMISSION,
+        Permission.GENERAL_USER_PERMISSION,
+        Permission.BLOCK_USER
+      ]
+    }
+    else if (user.role==='user')
+    {
+      result.permissions=[
+        Permission.GENERAL_USER_PERMISSION
+      ]
+    }
 
      return result
   }
@@ -22,7 +38,14 @@ async validateUser(username: string, password: string): Promise<any> {
   // If no user is found, try the admin repository
   const admin = await this.adminService.findOne(username);
   if (admin && await bcrypt.compare(password, admin.password)) {
-    const { password, ...result } = admin;
+    const { password, ...result } = admin as UserWithPermissions;
+    if(user.role==='admin'){
+      result.permissions=[
+        Permission.GENERAL_ADMIN_PERMISSION,
+        Permission.GENERAL_USER_PERMISSION,
+        Permission.BLOCK_USER
+      ]
+    }
     return result;
   }
 
